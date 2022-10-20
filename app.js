40642d5ce8793cd6431664f06c0d4c1748d991ca
eunha <dmsgk2323@naver.com>
2022-10-20 13:20:05 +0900

    메뉴에 아이템 추가되게 하는 기능 구현

--- app.js
+++ app.js
@@ -1,16 +1,35 @@
 function App () {
+    const $ = (selector) => document.querySelector(selector);
+    
     //form태그가 자동으로 전동되는걸 막아준다. 
-    document.querySelector("#espresso-menu-form").addEventListener("submit", (e)=> {
+    $("#espresso-menu-form").addEventListener("submit", (e)=> {
         e.preventDefault();
     })
 
     //메뉴의 이름을 입력 받는 건
-    document.querySelector("#espresso-menu-name").addEventListener("keypress", (e)=> {
+    $("#espresso-menu-name").addEventListener("keypress", (e)=> {
         if(e.key ==="Enter") {
-            console.log(document.querySelector("#espresso-menu-name").value);
-        }
-    })
+            const espressoMenuName = $("#espresso-menu-name").value
+            const menuItemTemplate = (espressoMenuName) => {return`<li class="menu-list-item d-flex items-center py-2">
+            <span class="w-100 pl-2 menu-name">${espressoMenuName}</span>
+            <button
+              type="button"
+              class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
+            >
+              수정
+            </button>
+            <button
+              type="button"
+              class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
+            >
+              삭제
+            </button>
+          </li>`;
+        };
+
+        $("#espresso-menu-list").insertAdjacentHTML("beforeend", menuItemTemplate(espressoMenuName)) ;
+    }})
 
 }
 
-App ()
+App()
